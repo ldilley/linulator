6b9d73355f3462dd569e353b94c558da8d9b35eb
Lloyd Dilley <ldilley@users.noreply.github.com>
2014-10-14 22:23:00 -0400

    More baseline OS import stuff

--- tools/Baseline.java
+++ tools/Baseline.java
@@ -18,24 +18,55 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
 import java.nio.file.Files;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.PosixFileAttributes;
 import java.nio.file.attribute.PosixFilePermission;
 import java.nio.file.attribute.PosixFilePermissions;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+// ToDo: Create Entry objects to populate database and write them to the filesystem table.
 
 // This class creates the initial system baseline from an existing Linux filesystem
 class Baseline
 {
+  public static final long SECONDS_IN_A_YEAR = 31556926;
+
   public static void main(String[] args)
   {
+    // Due to use of NIO
+    double javaVersion = Double.valueOf(System.getProperty("java.specification.version"));
+    if(javaVersion < 1.7)
+    {
+      System.err.println("Java >=7 is required.");
+      System.exit(1);
+    }
+
     if(!System.getProperty("os.name").equals("Linux"))
     {
       System.err.println("This must be run on a Linux system.");
+      System.exit(1);
+    }
+
+    if(!System.getProperty("user.name").equals("root"))
+    {
+      System.err.println("This must be run as root.");
       System.exit(1);
     }
 
@@ -54,7 +85,16 @@ class Baseline
     }
 
     if(args[0].equals("-c"))
-      createBaseline();
+    {
+      try
+      {
+        createBaseline();
+      }
+      catch(IOException ioe)
+      {
+        System.err.println(ioe.getMessage());
+      }
+    }
     else if(args[0].equals("-h"))
       showUsage();
     else
@@ -65,12 +105,12 @@ class Baseline
     }
   }
 
-  public static void createBaseline()
+  public static void createBaseline() throws IOException
   {
     Scanner scanner = new Scanner(System.in);
     String input = null;
 
-    System.out.println("Warning: This process should only be performed on a fresh install to avoid importing personal data!");
+    System.out.println("Warning: This should only be performed on a fresh install to avoid importing personal data!");
     System.out.print("Type \"yes\" and press enter if you understand this and want to proceed: ");
     input = scanner.nextLine();
 
@@ -80,26 +120,139 @@ class Baseline
       System.exit(0);
     }
 
-    //importFiles("/");
-    importFiles("/bin");
-    importFiles("/boot");
-    importFiles("/dev");
-    importFiles("/etc");
-    importFiles("/home");
-    importFiles("/lib");
-    importFiles("/lib64");
-    importFiles("/media");
-    importFiles("/mnt");
-    importFiles("/opt");
-    importFiles("/proc");
-    importFiles("/root");
-    importFiles("/sbin");
-    importFiles("/selinux");
-    importFiles("/srv");
-    importFiles("/sys");
-    importFiles("/tmp");
-    importFiles("/usr");
-    importFiles("/var");   
+    FileVisitor<Path> fileProcessor = new ProcessFile();
+    Files.walkFileTree(Paths.get("/"), fileProcessor);     // get everything under /
+  }
+
+  private static final class ProcessFile extends SimpleFileVisitor<Path>
+  {
+    @Override public FileVisitResult visitFile(Path file, BasicFileAttributes basicAttribs) throws IOException
+    {
+      PosixFileAttributes posixAttribs = Files.readAttributes(file, PosixFileAttributes.class, LinkOption.NOFOLLOW_LINKS);
+      Set<PosixFilePermission> posixPerms = posixAttribs.permissions();
+      String perms = PosixFilePermissions.toString(posixPerms);         // convert to symbolic format
+
+      String inode = posixAttribs.fileKey().toString();
+      Pattern p = Pattern.compile("\\=(\\d+)\\)");                      // regex to acquire just the inode
+      Matcher m = p.matcher(inode);
+      m.find();
+      inode = m.group(1);
+
+      long mtimeStamp = posixAttribs.lastModifiedTime().toMillis();
+
+      if(posixAttribs.isSymbolicLink())
+        System.out.println(Files.readSymbolicLink(file).toString());
+
+      // Only files have a type (not directories)
+      String type = Files.probeContentType(file).substring(0, 4);       // check for "text"
+      String contents = null;
+
+      if(type.equals("text"))
+        contents = readText(file);
+      else if(type.equals("inod"))
+        System.out.println("File is a link.");
+      else
+        contents = readBin(file);
+
+      // Log this to file once in production
+      System.out.println("Properties of file: " + file);
+      System.out.println(inode + " " + perms + " " + posixAttribs.owner().getName() + ":" + posixAttribs.group().getName() + " " + posixAttribs.size() + " " + calcMtime(mtimeStamp) + "\n");
+
+      return FileVisitResult.CONTINUE;
+    }
+
+    @Override public FileVisitResult preVisitDirectory(Path directory, BasicFileAttributes basicAttribs) throws IOException
+    {
+      PosixFileAttributes posixAttribs = Files.readAttributes(directory, PosixFileAttributes.class, LinkOption.NOFOLLOW_LINKS);
+      Set<PosixFilePermission> posixPerms = posixAttribs.permissions();
+      String perms = PosixFilePermissions.toString(posixPerms);
+
+      String inode = posixAttribs.fileKey().toString();
+      Pattern p = Pattern.compile("\\=(\\d+)\\)");
+      Matcher m = p.matcher(inode);
+      m.find();
+      inode = m.group(1);
+
+      long mtimeStamp = posixAttribs.lastModifiedTime().toMillis();
+
+      if(posixAttribs.isSymbolicLink())
+        System.out.println(Files.readSymbolicLink(directory).toString());
+
+      // Log this to file once in production
+      System.out.println("Properties of directory: " + directory);
+      System.out.println(inode + " " + perms + " " + posixAttribs.owner().getName() + ":" + posixAttribs.group().getName() + " " + posixAttribs.size() + " " + calcMtime(mtimeStamp) + "\n");
+
+      return FileVisitResult.CONTINUE;
+    }
+  }
+
+  public static String calcMtime(long mtimeStamp)
+  {
+    Date currentDate = new Date();
+    Date mdate = new Date(mtimeStamp);
+    long delta = currentDate.getTime() / 1000 - mdate.getTime() / 1000;
+    SimpleDateFormat timestampFormat;
+
+    // If mtime is >1 year, show MMM d YYYY instead
+    if(delta >= SECONDS_IN_A_YEAR)
+      timestampFormat = new SimpleDateFormat("MMM d  YYYY");
+    else
+      timestampFormat = new SimpleDateFormat("MMM d HH:mm");
+
+    return timestampFormat.format(mdate);
+  }
+
+  public static String readBin(Path passedFile)
+  {
+    String data = null;
+    File file = new File(passedFile.toString());
+    FileInputStream targetFile;
+
+    try
+    {
+      targetFile = new FileInputStream(file);
+      byte[] content = new byte[(int)file.length()];
+      targetFile.read(content);
+      data = new String(content);
+      targetFile.close();
+    }
+    catch(IOException ioe)
+    {
+      System.err.println("Unable to read " + passedFile.toString() + ":");
+      System.err.println(ioe.getMessage());
+    }
+
+    return data;
+  }
+
+  public static String readText(Path passedFile)
+  {
+    String data = null;
+
+    BufferedReader targetFile;
+    String line;
+
+    try
+    {
+      targetFile = new BufferedReader(new FileReader(passedFile.toString()));
+      while((line = targetFile.readLine()) != null)
+      {
+        data += line;
+      }
+      targetFile.close();
+    }
+    catch(IOException ioe)
+    {
+      System.err.println("Unable to read " + passedFile.toString() + ":");
+      System.err.println(ioe.getMessage());
+    }
+
+    return data;
+  }
+
+  public static void writeDatabase(Entry item)
+  {
+    // 
   }
 
   public static void showUsage()
@@ -110,32 +263,8 @@ class Baseline
     System.out.println("-h\tDisplays this help information\n");
   }
 
-  public static void importFiles(String directory)
+  class Entry
   {
-    // Stopping here for now to play some video games...
-    System.out.println("Not fully implemented yet.");
-    System.exit(0);
-
-    System.out.println("Importing " + directory + "...");
-
-    try
-    {
-      Path path = Paths.get(directory);
-      PosixFileAttributes attributes = Files.readAttributes(path, PosixFileAttributes.class);
-      System.out.println(attributes.fileKey() + " " + attributes.creationTime());
-      //Set<PosixFilePermission> set = Files.getPosixFilePermissions(path);
-      //Set<PosixFileAttributes> set = Files.getPosixFileAttributes(path);
-      //System.out.println(directory + ": " + PosixFilePermissions.toString(set));
-      
-    }
-    catch(IOException ioe)
-    {
-      // keep going or exit?
-    }
-  }
-
-  class File
-  {
-    // file attributes
+    // file properties that are shoved into the database
   }
 }
